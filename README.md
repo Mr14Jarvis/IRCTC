ff70d3e67dcc8cd668161d918c0f03b7c0b6f3b1
Lalit Singh Dhami <lalitsinghdhami7@gmail.com>
2024-07-05 20:52:04 +0530

    Main

---
+++ README.md
@@ -0,0 +1,5 @@
+<<<<<<< HEAD
+# IRCTC-
+=======
+# IRCTC- 
+>>>>>>> origin/main
---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+$title = 'Online Ticket Reservation System';
+//E-Ticketing System For Railway
+$supervisor_name = "LALIT SINGH DHAMI";
+$developer_name = "LALIT SINGH DHAMI";
+$developer_matric = "16/52HA016";
